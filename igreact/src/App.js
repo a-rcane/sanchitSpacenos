169639b4f1c000c18617daeff5a2ddf74b568ea4
a-rcane <73655772+a-rcane@users.noreply.github.com>
2022-07-07 12:18:20 +0530

    Basic layout

---
+++ igreact/src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header.js"
+
+const App = () => {
+    return(
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default App;
---
+++ igreact/src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    return(
+        <div className = "Header">
+            <navbar>
+                <ul>
+                    <li>
+                        <a class="active" href = "#home">profileViewer</a>
+                    </li>
+                </ul>
+            </navbar>
+            <div className = "back">
+            </div>
+        </div>
+    )
+}
+
+export default Header;
